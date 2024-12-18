18dfcc2f579b07fc03ac95df81482cd304108eb5
HaYoung Jung <134265313+vHaYoungv@users.noreply.github.com>
2024-12-18 14:21:05 +0900

    Title: 중앙값 계산 2, Time: 285ms, Memory: 16MB, Status: Passed - CodeTree

--- 241218/중앙값 계산 2/get-median-2.java
+++ 241218/중앙값 계산 2/get-median-2.java
@@ -5,8 +5,10 @@ public class Main {
         int n = sc.nextInt();
         sc.nextLine();
         int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for(int i=0; i<n; i=i+2){
-            System.out.print(arr[i/2] + " ");
+        for(int i=0; i<n; i+=2){
+            int[] subArr = Arrays.copyOfRange(arr, 0, i+1);
+            Arrays.sort(subArr);
+            System.out.print(subArr[i/2] + " ");
         }
     }
 }
