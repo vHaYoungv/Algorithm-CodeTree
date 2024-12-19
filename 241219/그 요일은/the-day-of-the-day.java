799d25c8dcad003fd32a3a6afa05adb359fad4ff
HaYoung Jung <134265313+vHaYoungv@users.noreply.github.com>
2024-12-19 11:32:05 +0900

    Title: 그 요일은, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241219/그 요일은/the-day-of-the-day.java
+++ 241219/그 요일은/the-day-of-the-day.java
@@ -12,7 +12,8 @@ public class Main {
 
         int cnt = 0;
         if (days-startDay>=0){
-            cnt = (days-startDay)/7 + 1;
+            cnt = (days-startDay)/7+1;
+            if(startDay==0) {cnt -= 1;}
         }
         System.out.println(cnt);
     }
